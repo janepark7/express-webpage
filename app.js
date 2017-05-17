bcd32c1d148ac3849056037b377d6d79d64d7811
Jane <janeparkjp@gmail.com>
2017-05-16 21:13:01 -0400

    third commit, country app

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const countries = require("./json/countries.json");
+console.log(countries);
 
 app.set("view engine", "ejs");
 app.use(express.static("assets"));
@@ -21,17 +23,28 @@ app.get("/link", function(req, res) {
 */
 //	Jane Park Webpage Template
 
+
+app.get("/imagegallery", function(req, res) {
+	res.render("imagegallery.ejs");
+});
+
+
+app.get("/countries", function(req, res) {
+
+var country = countries.USA;
+	console.log(country);
+
+	res.render("countries.ejs", country);
+});
+
 app.get("/", function(req, res) {
 	res.render("template", {
 		page: "janepark",
 		imagegallery: "http://localhost:3000/imagegallery",
 		github: "http://www.github.com/janepark7",
 		linkedin:"https://www.linkedin.com/in/jane-park-7aa208132/",
+		countries: "http://localhost:3000/countries",
 	});
-});
-
-app.get("/imagegallery", function(req, res) {
-	res.render("imagegallery.ejs");
 });
 
 app.listen(3000, function() {
