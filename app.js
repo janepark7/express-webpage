37b06ba549e46d0adae739c184a7b389335c7bf1
Jane <janeparkjp@gmail.com>
2017-05-18 16:17:16 -0400

    uncomment country commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
-// const countries = require("./json/countries.json");
-// console.log(countries);
+const countries = require("./json/countries.json");
+console.log(countries);
 
 app.set("view engine", "ejs");
 app.use(express.static("assets"));
@@ -35,20 +35,21 @@ app.get("/imagegallery", function(req, res) {
 	});
 });
 
-//change the app.get and create a function
+//	incorporating countries onto template
 
-// app.get("/countries:code", function(req, res) {
-// 	var country = countries[req.params.code];
-// 	console.log(country);
-// 	res.render("template", "countries.ejs", country);
-//	if (!countries) {
-// 		res.status(404);
-// 		return res.send("This is not a country");
-// }
-//
-// //Otherwise, render the admin....
-// res.render("admin");
-// });
+app.get("/countries:code", function(req, res) {
+	var country = countries[req.params.code];
+	console.log(countries);
+	res.render("template", {
+		page: "page/countries.ejs",
+	});
+	if (!country) {
+		res.status(404);
+		return res.send("This is not a country");
+}
+//Otherwise, render the admin....
+res.render("admin");
+});
 
 app.listen(3000, function() {
 	console.log("Your server is available at localhost:3000!");
