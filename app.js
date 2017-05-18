34289133858aa63ed204cc972501cda91f897177
Jane <janeparkjp@gmail.com>
2017-05-17 20:00:43 -0400

    fourth commit

--- app.js
+++ app.js
@@ -23,25 +23,32 @@ app.get("/link", function(req, res) {
 */
 //	Jane Park Webpage Template
 
-//change the app.get and create a function
-
-// app.get("/countries", function(req, res) {
-// 	var country = countries.GBR;
-// 	console.log(country);
-// 	res.render("countries.ejs", country);
-// });
-
-
 app.get("/", function(req, res) {
 	res.render("template", {
 		page: "janepark",
-		imagegallery: "imagegallery",
 	});
 });
 
 app.get("/imagegallery", function(req, res) {
-	res.render("imagegallery");
+	res.render("template", {
+		page: "page/imagegallery",
+	});
 });
+
+//change the app.get and create a function
+
+// app.get("/countries:code", function(req, res) {
+// 	var country = countries[req.params.code];
+// 	console.log(country);
+// 	res.render("template", "countries.ejs", country);
+//	if (!countries) {
+// 		res.status(404);
+// 		return res.send("This is not a country");
+// }
+//
+// //Otherwise, render the admin....
+// res.render("admin");
+// });
 
 app.listen(3000, function() {
 	console.log("Your server is available at localhost:3000!");
